93abcbdbd5518e98efe8732d2d45a79456500aa2
maoyinyan3071 <maoyinyan3071@gmail.com>
2025-02-23 19:12:35 +0900

    modify env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+// vite-env.d.ts
+interface ImportMetaEnv {
+    readonly VITE_GOOGLE_CLIENT_ID: string;
+    readonly VITE_GOOGLE_CLIENT_SECRET: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
